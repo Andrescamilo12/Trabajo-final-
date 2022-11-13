77b07bfce8b29d3c69239f73ae7c773085cc80c4
Camilo Andres Pulistar Lopez <111613926+Andrescamilo12@users.noreply.github.com>
2022-11-13 14:57:11 -0500

    Update ejer2.16.cs

--- CAP 2/ejer2.16.cs
+++ CAP 2/ejer2.16.cs
@@ -1,24 +1,24 @@
-espacio de nombres  ejericio4
+namespace ejericio
 {
-     programa de clases  publicas
+    public class program
     {
-         vacío estático  público principal () 
+        public static void Main()
         {
 
-             contador int ;
-             límite int ;
-            int  sumatoria ;
-            sumatoria  =  0 ;
-            contador  =  1 ;
-            limite  =  10 ;
+            int contador;
+            int limite;
+            int sumatoria;
+            sumatoria = 0;
+            contador = 1;
+            limite = 10;
 
 
-            for ( contador  =  1 ; contador  <=  limite ; contador ++ )
+            for (contador = 1; contador <= limite; contador++)
             {
-                sumatoria  =  sumatoria  +  contador ;
+                sumatoria = sumatoria + contador;
             }
 
-            consola _ WriteLine ( $" la suma es igual:{ sumatoria } " );
+            Console.WriteLine($"la suma es igual:{sumatoria}");
 
 
 
