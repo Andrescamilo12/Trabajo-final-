dc22beadae308a5a8cf41af561d775ac7ab30d5c
Camilo Andres Pulistar Lopez <111613926+Andrescamilo12@users.noreply.github.com>
2022-11-13 14:29:51 -0500

    Create ejer2.15.cs

---
+++ CAP 2/ejer2.15.cs
@@ -0,0 +1,20 @@
+namespace Ejercicio
+{
+    public class program
+    {
+        public static void Main()
+        {
+
+            double cuadrado;
+            int numero;
+
+            numero = 821;
+
+            cuadrado = Math.Pow(numero, 2);
+
+            Console.WriteLine($"El cuadrado de 821 es:{cuadrado}");
+
+
+        }
+    }
+}
