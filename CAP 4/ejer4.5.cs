ef21772fd8eaa7a48ae01b4128ef78b46d9a92b6
Camilo Andres Pulistar Lopez <111613926+Andrescamilo12@users.noreply.github.com>
2022-11-13 15:51:56 -0500

    Create ejer4.5.cs

---
+++ CAP 4/ejer4.5.cs
@@ -0,0 +1,85 @@
+namespace dates
+{
+    public class program
+    {
+        static void Main()
+        {
+            int anyo;
+            int mes;
+            int dia;
+            int dia1;
+
+            Console.WriteLine("Digite el año");
+            anyo = Convert.ToInt32(Console.ReadLine());
+
+            if (anyo % 4 == 0 || anyo % 100 != 0 || anyo % 400 == 0)
+            {
+
+                Console.WriteLine("");
+                Console.WriteLine("Año biciesto");
+                Console.WriteLine("Digite el mes");
+                mes = Convert.ToInt32(Console.ReadLine());
+
+                if (mes <=12 )
+                {
+                    Console.WriteLine("Digite el dia");
+                    dia = Convert.ToInt32(Console.ReadLine());
+                    if (dia <= 31)
+                    {
+                        dia = dia + 0;
+                        dia1 = dia + 1;
+                        
+
+                        Console.WriteLine("El dia que digito  es: " + dia + ":" + mes + ":" + anyo);
+                        Console.WriteLine("y");
+                        Console.WriteLine("El dia siguente es: " + dia1+ ":" + mes + ":" + anyo);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Este dia no fue encontrado");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Este Mes no fue encontrado");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Año no biciesto");
+                Console.WriteLine("Digite el mes");
+                mes = Convert.ToInt32(Console.ReadLine());
+
+                if (mes <= 12)
+                {
+                    Console.WriteLine("Digite el dia");
+                    dia = Convert.ToInt32(Console.ReadLine());
+                    if (dia <=31)
+                    {
+                        dia = dia + 0;
+                        dia1 = dia + 1;
+
+
+                        Console.WriteLine("El dia que digito  es: " + dia + ":" + mes + ":" + anyo);
+                        Console.WriteLine("y");
+                        Console.WriteLine("El dia siguente es: " + dia1 + ":" + mes + ":" + anyo);
+
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Este dia no fue  encontrado");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Este mes no fue  encontrado");
+                }
+            }
+        }
+    }
+
+}
+Footer
+© 2022 GitHub, I
