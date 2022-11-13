d9e30f2c4133901fa09d1ddb84cdd2d6d888a4c2
Camilo Andres Pulistar Lopez <111613926+Andrescamilo12@users.noreply.github.com>
2022-11-13 14:20:46 -0500

    Create ejer2.3.cs

---
+++ CAP 2/ejer2.3.cs
@@ -0,0 +1,19 @@
+namespace Ejercicios1
+{
+    public class program
+    {
+        public static void Main ()
+        {
+            int numero;
+            int resultado;
+            int doblado;
+
+            Console.WriteLine(" Por favor ingrese un numero ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            doblado = numero * 2;
+            resultado = doblado * 25;
+            Console.WriteLine($"el numero doblado es: {doblado}");
+            Console.WriteLine($"El resultado de la operacion es: {resultado}");
+        }
+    }
+}
